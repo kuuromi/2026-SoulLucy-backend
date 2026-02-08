ce44fff4ee1e91bd4f46c970f570237d01c4d71d
kuuromi <rosaliaarlusi@gmail.com>
2026-02-08 23:15:39 +0700

    feat: succesfully added sample room data to database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace _2026_SoulLucy_backend.Data
+﻿using Microsoft.EntityFrameworkCore;
+using _2026_SoulLucy_backend.Models;
+
+namespace _2026_SoulLucy_backend.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Room> Rooms { get; set; }
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -2,5 +2,9 @@
 {
     public class Room
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Capacity { get; set; }
     }
 }
